1d23ba493f0948360d6af48f00ff86d8eee7b5bf
khk <46013769+11Karl11@users.noreply.github.com>
2020-04-20 20:30:05 +0800

    druid & hikari

---
+++ druid-datasource/src/main/java/com/karl/druid/datasource/controller/DruidStatController.java
@@ -0,0 +1,22 @@
+package com.karl.druid.datasource.controller;
+
+import com.alibaba.druid.stat.DruidStatManagerFacade;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author karl xie
+ * Created on 2020-04-20 20:09
+ */
+@RestController
+public class DruidStatController {
+
+    @GetMapping("/monitor/druid/stat")
+    @Deprecated
+    public Object druidStat(){
+        // `DruidStatManagerFacade#getDataSourceStatDataList()` 方法，可以获取所有数据源的监控数据。
+        // 除此之外，DruidStatManagerFacade 还提供了一些其他方法，你可以按需选择使用。
+        return DruidStatManagerFacade.getInstance().getDataSourceStatDataList();
+    }
+
+}
